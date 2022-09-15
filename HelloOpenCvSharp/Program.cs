e6753d0095e0775acbf505ddca053f4dd5af253c
yeonseong shim <shimyseong@gmail.com>
2022-09-16 02:23:37 +0900

    Add OpecCvSharp

---
+++ HelloOpenCvSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * [ C# OpenCvSharp 설치 ]
+ * 1. 해당 프로젝트를 선택 -> Nuget 패키지 관리 -> OpenCvSharp 설치
+ * 2. 운영체제에 맞는 런타임 패키지 OpenCvSharp.runtime.win 설치 ( OpenCvSharp와 동일한 버전이어야 한다.)
+ */
+namespace HelloOpenCvSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
